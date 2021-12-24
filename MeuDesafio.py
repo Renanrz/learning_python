7c6264bcfa98f0c224fa211c2647b4b5f8a99461
Renanrz <65151378+Renanrz@users.noreply.github.com>
2021-12-24 19:34:21 +1000

    Update MeuDesafio

--- MeuDesafio.py
+++ MeuDesafio.py
@@ -5,25 +5,17 @@ pygame.init()
 p1 = str(input('Digite aqui o nome de uma pessoa q mora no 206: '))
 if p1 == 'Renan':
     pygame.mixer.music.load('RR.mp3')
-    pygame.mixer.music.play()
-    pygame.event.wait()
 else:
     if p1 == 'Mayara':
         pygame.mixer.music.load('MB.mp3')
-        pygame.mixer.music.play()
-        pygame.event.wait()
     else:
         if p1 == 'Rai':
             pygame.mixer.music.load('RS.mp3')
-            pygame.mixer.music.play()
-            pygame.event.wait()
         else:
             if p1 == 'Dave':
                 pygame.mixer.music.load('Dave.mp3')
-                pygame.mixer.music.play()
-                pygame.event.wait()
             else:
                 pygame.mixer.music.load('other.mp3')
-                pygame.mixer.music.play()
-                pygame.event.wait()
 
+pygame.mixer.music.play()
+pygame.event.wait()
